c37b644d70d4a6099a70beefa0be84d5871a3900
N VINOD KUMAR <nvinodkumar384@gmail.com>
2026-08-24 10:27:29 +0530

    Initial Commit...

---
+++ src/main/java/com/vk/PetHub/PetHubApplication.java
@@ -0,0 +1,14 @@
+package com.vk.PetHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHubApplication {
+
+	public static void main(String[] args) {
+		System.out.println("running..");
+		SpringApplication.run(PetHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vk/PetHub/PetHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.PetHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
